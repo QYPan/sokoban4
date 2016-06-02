bdf0f1b3d1198030c2d7e9e05a70f3f2425f9ff5
pqy <QingYouPan1994@163.com>
2016-06-02 08:01:52 +0800

    fix

--- kill.c
+++ kill.c
@@ -3,11 +3,9 @@
 #include "ai.h"
 
 #define JBW(r, c) \
-	(tbox[r][c] == mele->box_p || tbox[r][c] == mele->wall_p)
+	(tbox[r][c] == mele->box_g || tbox[r][c] == mele->wall_g)
 
-char tbox[MAPSIZE][MAPSIZE];
-
-int judge_kill(MAPELE *mele, int br, int bc)
+int judge_kill(char tbox[][MAPSIZE], MAPELE *mele, int br, int bc)
 {
 	Coor Stack[1000];
 	int v[MAPSIZE][MAPSIZE];
@@ -17,61 +15,84 @@ int judge_kill(MAPELE *mele, int br, int bc)
 	int dead2 = 0;
 	int i, k;
 	int flag;
+	int beg;
+	int boxcount = 0;
+	int fillcount = 0;
+	memset(v, 0, sizeof(v));
+
+	v[br][bc] = 1;
+	if(NIL_BOX[br][bc] == mele->nil_box_g)
+		fillcount++;
+
 	for(k = 0; k < 4; k++){
 		if(JBW(br+gr[k], bc+gc[k])){
 			Coor dir;
-			memset(v, 0, sizeof(v));
-			v[br][bc] = 1;
 			dir.r = br+gr[k];
 			dir.c = bc+gc[k];
 			top = 0;
 			Stack[top++] = dir;
 			v[dir.r][dir.c] = 1;
+			if(tbox[dir.r][dir.c] == mele->box_g)
+				boxcount = 2;
+			if(NIL_BOX[dir.r][dir.c] == mele->nil_box_g)
+				fillcount++;
 			if(k < 2)
 				flag = 2;
 			else 
 				flag = 0;
-			while(top){
-				int beg;
-				Coor d = Stack[top];
-				if(tbox[d.r][d.c] == mele->wall_p){
+			while(top && !dead){
+				Coor d = Stack[top-1];
+				if(tbox[d.r][d.c] == mele->wall_g){
 					if(k < 2) dead1 = 1;
 					else dead2 = 1;
 					break;
 				}
-				vis[d.r][d.c] = 1;
 				beg = flag;
-				if()
 				for(i = beg; i < beg+2; i++){
 					Coor nd;
 					nd.r = d.r+gr[i];
 					nd.c = d.c+gc[i];
 					if(JBW(nd.r, nd.c) && !v[nd.r][nd.c]){
+						int tbeg;
+						if(flag == 2) tbeg = 0;
+						else tbeg = 2;
+						if(tbox[nd.r][nd.c] == mele->box_g && (v[nd.r+gr[tbeg]][nd.c+gc[tbeg]] || v[nd.r+gr[tbeg+1]][nd.c+gc[tbeg+1]])){
+							dead = 1;
+							break;
+						}
 						v[nd.r][nd.c] = 1;
 						Stack[top++] = nd;
+						if(tbox[nd.r][nd.c] == mele->box_g)
+							boxcount++;
+						if(NIL_BOX[nd.r][nd.c] == mele->nil_box_g)
+							fillcount++;
 						break;
 					}
 				}
 				if(i == beg+2){
 					top--;
 				}
-				if(beg == 0)
-					beg = 2;
+				if(flag == 0)
+					flag = 2;
 				else 
-					beg = 0;
+					flag = 0;
 			}
 		}
 	}
+	printf("fillcount: %d\n", fillcount);
+	printf("boxcount: %d\n", boxcount);
+	printf("dead: %d\n", dead);
+	printf("dead1: %d\n", dead1);
+	printf("dead2: %d\n", dead2);
+	if(fillcount == boxcount)
+		return 0;
+	if(dead || (dead1 && dead2))
+		return 1;
+	return 0;
 }
 
 int kill(char g[][MAPSIZE], MAPELE *mele, int br, int bc)
 {
-	int r, c;
-	for(r = 0; r < MAPSIZE; r++){
-		for(c = 0; c < MAPSIZE; c++){
-			tbox[r][c] = g[r][c];
-		}
-	}
-	if(judge_kill(mele, br, bc))
+	if(judge_kill(g, mele, br, bc))
 		return 1;
 }
